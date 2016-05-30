c05296128791e5b94e3ca75af4683f34c0dab303
dmygaien <dmygaenko@gmail.com>
2016-05-30 23:32:19 +0300

    added directive

--- app/app.js
+++ app/app.js
@@ -11,4 +11,37 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+}])
+
+.controller('testController', function ($scope) {
+
+  $scope.firstname = 'value 1';
+  $scope.lastname = 'value 2';
+
+  $scope.tweets = [
+    {author: 'author1', text: 'text1'},
+    {author: 'author2', text: 'text2'},
+    {author: 'author3', text: 'text3'}
+  ];
+
+  $scope.controllerFn = function (arg) {
+    console.log('testController.controllerFn ' + arg);
+  }
+})
+
+.directive('testDirective', function () {
+  return {
+    restrict: 'E',
+    scope: {
+      testDirectiveFn: '&',
+      testProperty: '@',
+    },
+    link: function ($scope, elem, attrs) {
+      $scope.runTestDirectiveFn = function() {
+        $scope.testDirectiveFn({arg: 'arg from directive'});
+      }
+    },
+    template: '<div><h3>test directive with {{testProperty}}</h3>' +
+    '<button ng-click="runTestDirectiveFn()">Change Data</button>',
+  }
+})
